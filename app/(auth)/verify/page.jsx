545394660b6e5abbf6caccaef894fc9e4d7e5e96
Juan SALAZAR <juanssal@gmail.com>
2023-10-02 08:52:29 +0200

    Send email verification (supabase)

---
+++ app/(auth)/verify/page.jsx
@@ -0,0 +1,8 @@
+export default function Verify() {
+  return (
+    <main className="text-center">
+        <h2>thanks for registering!</h2>
+        <p>Before logging in, you need to veryfy your email address</p>
+    </main>
+  )
+}
