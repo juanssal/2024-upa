4b69eca9b8cd431c8d11a140598d2b0e4bfe417c
Juan SALAZAR <juanssal@gmail.com>
2023-10-03 08:37:16 +0200

    Add protected routes

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,16 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link"
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
-export default function AuthLayout({ children }) {
-    return (
-        <>
-        {/* <nav> */}
-            <h2>Auth Layout</h2>
-            <Link href="/signup">Sign up</Link>
-            <Link href="/login">Log in</Link>
-        {/* </nav> */}
-            {children}
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
 
-        </>
-    )
+  if (data.session) {
+    redirect('/')
+  }
+
+  return (
+    <>
+      <nav>
+        <h1>Dojo Helpdesk</h1>
+        <Link href="/signup">Sign up</Link>
+        <Link href="/login">Login</Link>
+      </nav>
+      {children}
+    </>
+  )
 }
